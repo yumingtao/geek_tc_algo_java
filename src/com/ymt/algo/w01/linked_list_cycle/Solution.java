02311328c657dc31b7e382df6b2b62b78e65660d
ymingta <mingtao.yu@daimler.com>
2021-06-21 23:00:17 +0800

    Add comments and optimize condition.

--- src/com/ymt/algo/w01/linked_list_cycle/Solution.java
+++ src/com/ymt/algo/w01/linked_list_cycle/Solution.java
@@ -10,28 +10,29 @@ package com.ymt.algo.w01.linked_list_cycle;
 public class Solution {
     public boolean hasCycle(ListNode head) {
 
+        //排除空链表和一个节点链表的情况
         if (head == null || head.next == null) {
             return false;
         }
 
+        //快慢指针赋值
         ListNode fast = head.next.next;
         ListNode slow = head;
 
-        while (fast != null && slow != null) {
-            //有环的时候，fast后边一定还有节点
-            if ((fast.val == slow.val) && fast.next != null) {
+        //有环的时候，fast后边一定还有节点，所以需要判断fast.next != null
+        while (fast != null && fast.next != null && slow != null) {
+            if (fast.val == slow.val) {
                 return true;
             }
 
-            if (slow.next != null && fast.next != null && fast.next.next != null) {
-                fast = fast.next.next;
-                slow = slow.next;
-            }else{
-                //注意整个程序的终止条件，当最快或最慢的走到最后也没有找到环
+            //注意整个程序的终止条件，当最快或最慢的走到最后也没有找到环
+            //fast.next在外层循环判断过，这里可以不用判断
+            if (slow.next == null || fast.next.next == null) {
                 break;
             }
+            fast = fast.next.next;
+            slow = slow.next;
         }
-
         return false;
     }
 
