82b560d9e48e2a22fe27650338ec201e4c6e0340
yumingtao <yumingtao2008@gmail.com>
2021-08-06 10:45:04 +0800

    Add DP solution for best time to buy and sell stock ii.

---
+++ src/com/ymt/algo/w06/best_time_to_buy_and_sell_stock_ii/Solution.java
@@ -0,0 +1,66 @@
+package com.ymt.algo.w06.best_time_to_buy_and_sell_stock_ii;
+
+import java.util.Arrays;
+
+/**
+ * 思路: 使用动态规划
+ * 1. f[i][j]表示第i天，手上持有j股股票的收益，可以有三种策略
+ * - 什么都不做，f[i][j] = f[i-1][j]
+ * - 卖出，f[i][0] = Math.max(f[i - 1][0], f[i - 1][1] + adjustedPrices[i]);
+ * - 买入，f[i][1] = Math.max(f[i - 1][1], f[i - 1][0] - adjustedPrices[i]);
+ * 2.注意：该题中是f[i - 1][0] -adjustedPrices[i]，因为可以多次买卖，需要用之前收益减去adjustedPrices[i]
+ *
+ * @author yumingtao
+ * @date 2021/8/6 09:20
+ */
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        //调整价格下标从1开始
+        int[] adjustedPrices = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            adjustedPrices[i] = prices[i - 1];
+        }
+        //可以不用写，默认就是0
+        //adjustedPrices[0] = 0;
+
+        //定义第i天持有0/1股股票的最大收益
+        int[][] f = new int[n + 1][2];
+        //初始化，将不合法或是未计算过的情况设置为一个极小值
+        for (int i = 0; i < n + 1; i++) {
+            Arrays.fill(f[i], -1000000000);
+        }
+
+        //第0天不持有任何股票时候的收益是0
+        //第0天持有1股股票的时候收益是不合法，即f[0][1] = -1000000000
+        f[0][0] = 0;
+
+        //遍历所有状态
+        for (int i = 1; i < n + 1; i++) {
+            //遍历所有策略，0-不持有股票，表示卖出，1-持有股票，表示买入
+            for (int j = 0; j <= 1; j++) {
+                //什么都不做，该持有继续持有，不持有则继续观望
+                f[i][j] = f[i - 1][j];
+
+                //卖出的时候，会在[i-1][1]的基础上获得adjustedPrices[i]的收益
+                if (j == 0) {
+                    //因为在前边已经设置过什么也不做的情况，即f[i][0] = f[i - 1][0]
+                    //如果没有设置，此处需要将f[i][0]替换为f[i-1][0]
+                    //表示在维持现状和卖出股票之间选择一个收益最大的更新当前的状态
+                    f[i][0] = Math.max(f[i][0], f[i - 1][1] + adjustedPrices[i]);
+                }
+
+                //买入的时候，会在[i-1][0]的基础上减去adjustedPrices[i]，才是当前的收益
+                if (j == 1) {
+                    //因为在前边已经设置过什么也不做的情况，即f[i][1] = f[i - 1][1]
+                    //如果没有设置，此处需要将f[i][1]替换为f[i-1][1]
+                    //表示在维持现状和买入股票之间选择一个收益最大的更新当前的状态
+                    f[i][1] = Math.max(f[i][1], f[i - 1][0] - adjustedPrices[i]);
+                }
+            }
+        }
+
+        return f[n][0];
+    }
+}
