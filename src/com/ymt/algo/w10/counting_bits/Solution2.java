560c520c30e53429af29b109415b4c64fa05ae02
yumingtao <yumingtao2008@gmail.com>
2021-08-22 21:43:32 +0800

    Update solution2 for counting bits.

--- src/com/ymt/algo/w10/counting_bits/Solution2.java
+++ src/com/ymt/algo/w10/counting_bits/Solution2.java
@@ -9,7 +9,7 @@ public class Solution2 {
         int[] ans = new int[n + 1];
 
         //使用DP
-        ans[0] = 0;
+        //ans[0] = 0;
         for (int i = 1; i <= n; i++) {
             // 之前的统计相当于，用一个数，不停地减去lowbit，每减去一个，1的个数就+1
             // 反过来可以理解成，当前数字1的个数，是减去一个lowbit得到的数字中1的个数+1
